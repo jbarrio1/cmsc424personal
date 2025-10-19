359d055fb2e250c6fcb3c88e8b91791da9f77e2d
jbarrio <you@example.com>
2025-10-19 15:46:11 -0400

    13

--- proj2/queries.py
+++ proj2/queries.py
@@ -20,13 +20,15 @@ queries[2][0] = "select city"
 queries[2][1] = "group by airportid,city order by airportid"
 
 ### 3.
-### Explaination - 
+### Explaination - The issues arises from the where clause not filtering out a customer enteriely if they have a non null value after the join. That is
+## if a customer has 6 flights, 1 out of jfk, the where clause will remove/not keep that entry and the customer will have 5 flights, then the group by
+# executes, then the having does, and that customer that should've have been removed entirely has <=5 flights passing the having clause  
 ###
 queries[3] = """
 select cid 
 from customer_flights c left join flights_JFK j 
   on c.flightid = j.flightid 
-where j.flightid is null 
+where not exists (select * from customer_flights c2 left join flights_JFK j2 on c2.flightid = j2.flightid where c.cid = c2.cid and j2.flightid is not null)
 group by cid 
 having count(*) <= 5;
 """
